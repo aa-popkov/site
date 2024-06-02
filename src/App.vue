95b97bef0eced5aa3276b41e06875b512e4b54f6
Alexey Popkov <AlexeyPopkov@pm.me>
2024-06-02 17:38:16 +0300

    Add robors and sitemap

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,33 @@ import PageFooter from '@/components/PageFooter.vue'
 import PageLoader from '@/components/Shared/PageLoader.vue'
 import { useUserStore } from '@/utils/store'
 import NotificationSection from '@/components/Shared/NotificationSection.vue'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 
-const store = useUserStore()
+const { loading } = useUserStore()
+const router = useRouter()
+
+onMounted(() => {
+  router.afterEach((to) => {
+    if (document) {
+      document.title = (to.meta.title as string) || 'My Portfolio'
+      document
+        .querySelector('meta[name="description"]')
+        ?.setAttribute(
+          'content',
+          (to.meta.description as string) || 'Description of my portfolio site'
+        )
+      document
+        .querySelector('meta[name="keywords"]')
+        ?.setAttribute('content', (to.meta.keywords as string) || 'portfolio, vue, developer')
+    }
+  })
+})
 </script>
 
 <template>
   <div id="root" class="2xl:container mx-auto w-full">
-    <PageLoader v-if="store.loading" />
+    <PageLoader v-if="loading" />
     <PageHeader />
     <div class="pb-32 md:pb-24">
       <RouterView />
--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,12 @@ import HomePage from '@/pages/HomePage.vue'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: HomePage
+    component: HomePage,
+    meta: {
+      title: '🧔‍♂️ Home - Alexey Popkov',
+      description: 'Welcome to information stand about me',
+      keywords: 'portfolio, vue, developer, software, software engineer'
+    }
   },
   {
     path: '/pricing',
@@ -12,7 +17,12 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: '/contact',
-    component: () => import('@/pages/ContactPage.vue')
+    component: () => import('@/pages/ContactPage.vue'),
+    meta: {
+      title: '👋 Contact Me - Alexey Popkov',
+      description: 'Contact if you wanna',
+      keywords: 'contact, vue, developer, software, software engineer'
+    }
   }
 ]
 
