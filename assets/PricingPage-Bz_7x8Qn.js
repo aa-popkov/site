f72c6b3c7a6c1d9b0002e94ac8aef467d9cc7454
Alexey Popkov <AlexeyPopkov@pm.me>
2024-07-15 12:59:58 +0300

    fix to gh-pages

--- assets/PricingPage-Bz_7x8Qn.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c}from"./index-DCzGUcIl.js";const r={};function n(t,o){return a(),c("div",null,"This page made be available later!")}const l=e(r,[["render",n]]);export{l as default};
