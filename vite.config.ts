b6ece195261e7f7f974d02622a0a2537e8ef680c
Alexey Popkov <AlexeyPopkov@pm.me>
2024-07-15 12:03:24 +0300

    fix to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/portfolio/',
+  base: '/aapopkov_front_vue/',
   plugins: [
     vue(),
     vueJsx(),
